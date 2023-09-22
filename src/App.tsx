53053e3ccb5eee75d67c32fe9fb1c7a856840a7c
DienGitHub <trietnk01@gmail.com>
2023-09-22 20:09:45 +0700

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import LoadingSpinner from "ui-component/LoadingSpinner";
 import Locales from "ui-component/Locales";
 import Snackbar from "ui-component/Snackbar";
 const App = () => {
-	console.log("abc");
+	console.log("nguyenkimdien");
 	return (
 		<Locales>
 			<AuthProvider>
